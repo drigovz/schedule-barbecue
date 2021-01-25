196ff528fe6c6be251f2f52209cca09dfcfc6b33
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-01-25 17:56:17 -0300

    feat: add init entities definition and add BaseEntity

---
+++ Api.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        private DateTime? _createdAt;
+
+        public DateTime? CreatedAt
+        {
+            get { return _createdAt; }
+            set { _createdAt = (value == null ? DateTime.UtcNow : value); }
+        }
+    }
+}
