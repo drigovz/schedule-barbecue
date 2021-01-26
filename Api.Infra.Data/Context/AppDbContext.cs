d29a7d166590eb2270c5637d28a2aa839b8f122e
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-01-25 21:16:11 -0300

    feat: include DB Context configuration

---
+++ Api.Infra.Data/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Infra.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Barbecue> Barbecues { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+        }
+    }
+}
